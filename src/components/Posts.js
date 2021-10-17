901d9c21bb3e8e2b3a1a3f4fbf2a56d40d1be5ad
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2021-10-17 22:10:22 +0500

    added new post we write to data

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,6 +6,12 @@ class Posts extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
+    }
+  }
   render() {
     const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
@@ -25,9 +31,11 @@ class Posts extends Component {
 
 Posts.propTypes = {
   fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object
 };
 const mapStateToProps = (state) => ({
-  posts: state.posts.items
+  posts: state.posts.items,
+  newPost: state.posts.item
 });
 export default connect(mapStateToProps, { fetchPosts })(Posts);
